4997f00d97339a4bc8abec0db72fa83a947af042
daramayo90 <da.aramayo1990@gmail.com>
2022-08-23 15:19:53 -0500

    nextui implemented with darktheme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@nextui-org/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
     <>
-      <h1>Hello world!</h1>
+      <Button color="gradient">Hello world!</Button>
     </>
   );
 };

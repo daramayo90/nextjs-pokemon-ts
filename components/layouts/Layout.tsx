8f2b78d9a6fd074b27687aff5fb9c33483ac22b6
daramayo90 <da.aramayo1990@gmail.com>
2022-08-23 15:48:19 -0500

    implement navbar with src image imported

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -1,5 +1,6 @@
 import { FC, ReactNode } from "react";
 import Head from "next/head";
+import { Navbar } from "../ui";
 
 interface Props {
   children: ReactNode;
@@ -13,10 +14,16 @@ export const Layout: FC<Props> = ({ title, children }) => {
         <title>{title || "Pokemon App"}</title>
         <meta name="author" content="Damian Aramayo" />
         <meta name="description" content="Pokemons information" />
-        <meta name="keywords" content="XXXX, pokemon, pokedex" />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
       </Head>
-      {/**Navbar */}
-      <main>{children}</main>
+      <Navbar />
+      <main
+        style={{
+          padding: "0px 20px",
+        }}
+      >
+        {children}
+      </main>
     </>
   );
 };
